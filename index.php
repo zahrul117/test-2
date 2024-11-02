0d80e339044b93d8b07e001a731aea6151478206
zahrul117 <zahrul0009@gmail.com>
2024-11-02 07:41:50 +0700

    menambahkan baris baru lagi

--- index.php
+++ index.php
@@ -7,5 +7,10 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, dolores dolor alias natus itaque optio deleniti tempore saepe autem sint!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
